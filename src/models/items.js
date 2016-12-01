6a8d383283b4d1e7dd6cba2ff676ad1eec544041
Gloria Guy <totallygloria@gmail.com>
2016-12-01 09:10:27 -0800

    Started a list of in-game items with names and flavor text.

---
+++ src/models/items.js
@@ -0,0 +1,31 @@
+// List of all the non-weapon items in the game
+
+module.exports = [
+  {
+    name: 'banana',
+    description: 'Looks pretty tasty.',
+    value: 10
+  },
+  {
+    name: 'wine',
+    description: 'Twinkles red in the torchlight, smells like blackberry and oak',
+    value: 5
+  },
+  {
+    name: 'can of beans',
+    description: 'Might be edible, if you can get the can open...',
+    value: 1
+  },
+  {
+    name: 'pizza',
+    description: 'It\s not moldy, and it\s pizza, so it\s probably good. Right?',
+    value: 8
+  },
+  {
+    name: 'plate of cookies',
+    description: 'Doesn\'t seem to be anyone around to stop you...',
+    value: 9
+  },
+
+
+];
