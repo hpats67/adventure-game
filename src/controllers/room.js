9d44ac729a9435066a69a41d3754a0239ec3e5a4
Tim Combs <tim00combs@gmail.com>
2016-12-02 14:19:14 -0800

    add weaponMethods.js, gameMethods.js & gameController.js, refactor room.js which may go away

--- src/controllers/room.js
+++ src/controllers/room.js
@@ -1,4 +1,4 @@
-//import room from '../models/roomMethods';
+import roomMethods from '../models/roomMethods';
 import rooms from '../models/rooms';
 import itemMethods from '../models/itemMethods';
 import monsterMethods from '../models/monsterMethods';
@@ -6,15 +6,16 @@ import monsterMethods from '../models/monsterMethods';
 roomController.$inject = ['$scope'];
 
 export default function roomController($scope) {
-  let itemObjArr = itemMethods.get();
-  let monsterObject = monsterMethods.get();
+  // let itemObjArr = itemMethods.get();
+  // let monsterObject = monsterMethods.get();
 
-  $scope.roomTitle = rooms.Foyer.name;
-  $scope.roomDescription = rooms.Foyer.description;
-  // $scope.itemDescription = itemObject.description;
-  // $scope.itemName = itemObject.name;
-  $scope.items = itemObjArr;
-  $scope.monsterDescription = monsterObject.description;
-  $scope.monsterName = monsterObject.name;
+  // $scope.roomTitle = rooms.Foyer.name;
+  // $scope.roomDescription = rooms.Foyer.description;
+  // // $scope.itemDescription = itemObject.description;
+  // // $scope.itemName = itemObject.name;
+  // $scope.items = itemObjArr;
+  // $scope.monsterDescription = monsterObject.description;
+  // $scope.monsterName = monsterObject.name;
+  $scope.getRoom = roomMethods.getRoom;
 
 }
