a42b59accf7b5b32a03a5686075c13a92175e05e
Tim Combs <tim00combs@gmail.com>
2016-12-02 08:56:47 -0800

    update index.html, room.js & itemMethods to handle 1 or 2 items in an array

--- src/controllers/room.js
+++ src/controllers/room.js
@@ -6,13 +6,14 @@ import monsterMethods from '../models/monsterMethods';
 roomController.$inject = ['$scope'];
 
 export default function roomController($scope) {
-  let itemObject = itemMethods.get();
+  let itemObjArr = itemMethods.get();
   let monsterObject = monsterMethods.get();
 
   $scope.roomTitle = rooms.Foyer.name;
   $scope.roomDescription = rooms.Foyer.description;
-  $scope.itemDescription = itemObject.description;
-  $scope.itemName = itemObject.name;
+  // $scope.itemDescription = itemObject.description;
+  // $scope.itemName = itemObject.name;
+  $scope.items = itemObjArr;
   $scope.monsterDescription = monsterObject.description;
   $scope.monsterName = monsterObject.name;
 
