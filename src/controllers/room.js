23b125debfca6446c7d69f0d07718dcefe780ce3
Tim Combs <tim00combs@gmail.com>
2016-12-01 18:29:26 -0800

    room is now populated with items, monster & room, need to move to game controller

--- src/controllers/room.js
+++ src/controllers/room.js
@@ -1,14 +1,20 @@
 //import room from '../models/roomMethods';
 import rooms from '../models/rooms';
 import itemMethods from '../models/itemMethods';
+import monsterMethods from '../models/monsterMethods';
 
 roomController.$inject = ['$scope'];
 
 export default function roomController($scope) {
   let itemObject = itemMethods.get();
+  let monsterObject = monsterMethods.get();
 
   $scope.roomTitle = rooms.Foyer.name;
   $scope.roomDescription = rooms.Foyer.description;
   $scope.itemDescription = itemObject.description;
   $scope.itemName = itemObject.name;
+  $scope.monsterDescription = monsterObject.description;
+  $scope.monsterName = monsterObject.name;
+
+
 }
