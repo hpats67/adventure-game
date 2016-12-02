059e4904ecaf71e545bde5c63a7df109abfbab10
Gloria (Guy) Anholt <GloriaAnholt@users.noreply.github.com>
2016-12-02 14:23:52 -0800

    Took comments out -- can get from log if needed.

--- src/controllers/room.js
+++ src/controllers/room.js
@@ -6,16 +6,7 @@ import monsterMethods from '../models/monsterMethods';
 roomController.$inject = ['$scope'];
 
 export default function roomController($scope) {
-  // let itemObjArr = itemMethods.get();
-  // let monsterObject = monsterMethods.get();
 
-  // $scope.roomTitle = rooms.Foyer.name;
-  // $scope.roomDescription = rooms.Foyer.description;
-  // // $scope.itemDescription = itemObject.description;
-  // // $scope.itemName = itemObject.name;
-  // $scope.items = itemObjArr;
-  // $scope.monsterDescription = monsterObject.description;
-  // $scope.monsterName = monsterObject.name;
   $scope.getRoom = roomMethods.getRoom;
 
 }
