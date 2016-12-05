6eb01e80abe9700f28b0aa8400e94c197a219dfe
Tim Combs <tim00combs@gmail.com>
2016-12-04 21:01:47 -0800

    refactor weaponMethods to return object, not array and refactor weaponMethods.test to reflect change

--- src/models/weaponMethods.js
+++ src/models/weaponMethods.js
@@ -5,7 +5,7 @@ const weaponMethods = {};
 weaponMethods.get = function() {
   let index = Math.floor(Math.random() * (weapons.length + 1));
   let selectedWeapon = weapons.splice(index, 1);
-  return selectedWeapon; //selected weapon is an array of one object
+  return selectedWeapon[0]; //selected weapon is an array of one object
 };
 
 export default weaponMethods;
--- test/weaponMethods.test.js
+++ test/weaponMethods.test.js
@@ -6,14 +6,14 @@ import chai from 'chai';
 const {assert} = chai;
 
 describe('the get weapon method', () => {
-  const returnedArray = weaponMethods.get();
+  const returnedObj = weaponMethods.get();
 
   it('gets a weapon', () => {
-    assert.isOk(returnedArray[0].type, 'weaponMethods.get does not return an object with a type property');
+    assert.isOk(returnedObj.type, 'weaponMethods.get does not return an object with a type property');
   });
 
   it('removes weapon from weapons array', () => {
-    assert.notInclude(weapons, returnedArray[0], 'weaponMethods.get does not remove an object from array');
+    assert.notInclude(weapons, returnedObj, 'weaponMethods.get does not remove an object from array');
   });
 
 });
