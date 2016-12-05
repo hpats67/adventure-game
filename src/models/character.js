a18ab7c69c950b87f6a23b82a0b3b6ae635f53a4
Steven Jackson <hpats67@gmail.com>
2016-12-04 20:43:04 -0800

    character Items are clickable and update DOM

--- src/models/character.js
+++ src/models/character.js
@@ -2,7 +2,38 @@ module.exports = {
 
   name: 'Adventurer',
   hp: 100,
-  inventory: [],
+  inventory: [{
+    type: 'item',
+    name: 'yellow banana',
+    description: 'Looks pretty tasty.',
+    value: 10
+  },
+  {
+    type: 'item',
+    name: 'wine',
+    description: 'Twinkles red in the torchlight, smells like blackberry and oak',
+    value: 5
+  },
+  {
+    type: 'weapon',
+    name: 'torch',
+    description: 'This seems handy for getting around in dark damp places! Careful not to burn yourself.',
+  },
+  {
+    type: 'weapon',
+    name: 'salt shaker',
+    description: 'Standard glass shaker, with some salt and rice in it. Might be useful in a fight.',
+  },
+  {
+    type: 'weapon',
+    name: 'catnip sachet',
+    description: 'Smells a little minty and herby. A kitty would love this.',
+  },
+  {
+    type: 'weapon',
+    name: 'golden carrot',
+    description: 'A heavy, golden carrot. Definitely not edible, possibly valuable if it\'s real gold.',
+  }],
   isDead: false
 
 };
