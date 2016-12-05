540f6bcbad2daba5d4ea53277f3eee355bc087bc
Gloria (Guy) Anholt <GloriaAnholt@users.noreply.github.com>
2016-12-05 14:14:07 -0800

    Added comments to be consistent

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -7,21 +7,24 @@ import character from '../models/character';
 gameController.$inject = ['$scope'];
 
 export default function gameController($scope) {
+  // Initialize the game to start in the Foyer
   $scope.characterHealth = character.hp;
-  $scope.currentRoom = rooms.Foyer;
   $scope.inventory = character.inventory;
+  $scope.currentRoom = rooms.Foyer;
   $scope.roomInv = rooms.Foyer.inventory;
 
-  //when door is clicked
+  // On door click
   $scope.changeRoom = function(currRoom, direction) {
     $scope.currentRoom = roomMethods.getRoom(currRoom, direction);
   };
 
+  // If user clicks monster
   $scope.monsterClick = function(monster) {
     gameMethods.hurtCharacter(monster);
     $scope.characterHealth = character.hp;
   };
 
+  // On item in room click
   $scope.itemClick = function(item, roomAreaNum, currRoom) {
     charMethods.pickUpItem(item, currRoom);
     $scope.roomInv = currRoom.inventory;
