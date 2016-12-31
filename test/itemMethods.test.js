45d123f517136aa1609e79d8107934b7033a2142
Gloria Guy <totallygloria@gmail.com>
2016-12-30 17:23:40 -0800

    Updates test to check all of returned item's properties
    Fixes typos and updates style to match other tests. Passes.

--- test/itemMethods.test.js
+++ test/itemMethods.test.js
@@ -1,19 +1,23 @@
+/* globals chai */
+
 import itemMethods from '../src/models/itemMethods';
 import items from '../src/data/items';
 
-import chai from 'chai';
-
-const {assert} = chai;
+const { assert } = chai;
 
 describe('the get item method', () => {
-  const returnedArray = itemMethods.get();
+  const returnedItem = itemMethods.get();
 
   it('gets an item', () => {
-    assert.isOk(returnedArray[0].type, 'weaponMethods.get does not return an object with a type property');
+    // getting a weapon should return an object with our weapon properties
+    assert.isOk(returnedItem.type, 'weaponMethods.get does not return an object with a type property');
+    assert.isOk(returnedItem.name, 'weaponMethods.get does not return an object with a type property');
+    assert.isOk(returnedItem.description, 'weaponMethods.get does not return an object with a type property');
+    assert.isOk(returnedItem.value, 'weaponMethods.get does not return an object with a type property');
   });
 
   it('removes item from items array', () => {
-    assert.notInclude(items, returnedArray[0], 'itemMethods.get does not remove an object from array');
+    assert.notInclude(items, returnedItem, 'itemMethods.get does not remove an object from array');
   });
 
 });
